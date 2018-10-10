1e95a37907072428ad5c41ee79df775365f9281e
Luis Angel Mojena Román <lamojena@estudiantes.uci.cu>
2018-10-10 16:41:52 +0000

    Add NeutrosophicSetElement tests

--- tests/neutrosophic_set_test.rb
+++ tests/neutrosophic_set_test.rb
@@ -1,6 +1,37 @@
 require_relative '../neutrosophy/neutrosophic_set'
 require 'test/unit'
 
+class NeutrosophicSetElementTest < Test::Unit::TestCase
+
+  def setup
+    @svn_number = SVNNumber.new(0.2, 0.3, 0.5)
+    @nse = NeutrosophicSetElement.new('test', @svn_number)
+  end
+
+  def test_constructor_ok
+    assert_equal @nse.label, 'test'
+    assert_equal @nse.svn_number, @svn_number
+  end
+
+  def test_set_svnnumber_failure
+    assert_raise (ArgumentError) {NeutrosophicSetElement.new('test', 'a')}
+    assert_raise (ArgumentError) {NeutrosophicSetElement.new('test', 0.1)}
+    assert_raise (ArgumentError) {NeutrosophicSetElement.new('test', [0.1, 0.2, 0.5])}
+  end
+
+  def test_set_svnnumber_failure2
+    assert_raise(ArgumentError) {@nse.svn_number = 'a'}
+    assert_raise(ArgumentError) {@nse.svn_number = 0.1}
+    assert_raise(ArgumentError) {@nse.svn_number = [0.1, 0.2, 0.5]}
+  end
+
+  def test_set_svnnumber_success
+    assert_equal @nse.label, 'test'
+    assert_equal @nse.svn_number, @svn_number
+  end
+
+end
+
 class NeutrosophicSetTest < Test::Unit::TestCase
 
   def test_add
@@ -19,13 +50,8 @@ class NeutrosophicSetTest < Test::Unit::TestCase
 
   end
 
-  def test_to_array_order_of_insertion
+  def test_to_array_insertion_order
 
   end
-
-end
-
-
-class NeutrosophicSetElementTest < Test::Unit::TestCase
 
 end
