bbe71515b9cff065285a7a0a60efd1b922b82314
Luis Angel Mojena Román <lamojena@estudiantes.uci.cu>
2018-10-10 06:23:49 +0000

    Add unit tests for the neutrosophic_set module

---
+++ tests/neutrosophic_set_test.rb
@@ -0,0 +1,31 @@
+require_relative '../neutrosophy/neutrosophic_set'
+require 'test/unit'
+
+class NeutrosophicSetTest < Test::Unit::TestCase
+
+  def test_add
+
+  end
+
+  def test_remove
+
+  end
+
+  def test_get
+
+  end
+
+  def test_to_array
+
+  end
+
+  def test_to_array_order_of_insertion
+
+  end
+
+end
+
+
+class NeutrosophicSetElementTest < Test::Unit::TestCase
+
+end
